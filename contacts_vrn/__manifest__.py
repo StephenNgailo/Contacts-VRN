13e999bb20d0b41e31b25623254aa97a83946bbc
Stephen Ngailo <92292992+StephenNgailo@users.noreply.github.com>
2021-12-03 11:58:12 +0300

    [ADD] Contact VRN Module

---
+++ contacts_vrn/__manifest__.py
@@ -0,0 +1,18 @@
+
+{
+    "name": "Contact VRN/PON",
+    "summary": "Add VRN / Po No / DN fields ",
+    'description': """
+        - Add VRN (Vat Registration Number) field to contacts.
+        - Add Buyer Order Number (Po No) to Sales Order and Invoice.
+        - Add Delivery Note to Invoice.
+    """,
+    "version": "15.0.1.0.0",
+    "category": "Customer Relationship Management",
+    "author": "Stephen Ngailo, StiloTech Limited",
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+    "depends": ["base","sale_management"],
+    "data": ["views/res_partner.xml"],
+}
---
+++ contacts_vrn/models/res_partner.py
@@ -0,0 +1,48 @@
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
+
+class saleextend(models.Model):
+    _inherit = 'sale.order'
+
+    vrn = fields.Char(string="VRN",related="partner_id.vrn",readonly=True)
+    po_no = fields.Char('Buyer Order No')
+
+class partnerextend(models.Model):
+    _inherit = 'res.partner'
+
+    vrn = fields.Char('VRN')
+    vat = fields.Char(string='TIN', index=True, help="The Tax Identification Number. Complete it if the contact is subjected to government taxes. Used in some legal statements.")
+
+    _sql_constraints = [
+        ('unique_name', 'unique (name)', 'Customer/Supplier Name already exists!')
+    ]
+
+    # _sql_constraints = [
+    #     ('unique_vrn', 'unique (vrn)', 'Customer/Supplier VRN already exists!')
+
+class commpanyextend(models.Model):
+    _inherit = 'res.company'
+
+    vrn = fields.Char('Company VRN')
+    vat = fields.Char(related='partner_id.vat', string="TIN", readonly=False)
+
+class invoiceexted(models.Model):
+    _inherit = 'account.move'
+
+    vrn = fields.Char(string="VRN",related="partner_id.vrn",readonly=True)
+    po_no = fields.Char(string="Buyer Order No",related="sale_id.po_no")
+    delivery_ids = fields.Many2many('stock.picking', string='Delivery Note')
+
+    sale_id = fields.Many2one('sale.order',string="Sale Order", compute='compute_sale_id')
+
+    def compute_sale_id(self):
+        self.sale_id = False
+        if self.move_type == 'out_invoice':
+            if self.invoice_origin:
+                sale = self.env['sale.order'].search([['name','=',self.invoice_origin]],limit=1)
+                self.sale_id = sale.id
+        return
+
+
+	
