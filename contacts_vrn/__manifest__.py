2639c181cff700f62a9bb2d116225cdef5b9af77
Stephen Ngailo <92292992+StephenNgailo@users.noreply.github.com>
2021-12-03 14:40:27 +0300

    Update __manifest__.py

--- contacts_vrn/__manifest__.py
+++ contacts_vrn/__manifest__.py
@@ -7,14 +7,14 @@
         - Add Buyer Order Number (Po No) to Sales Order and Invoice.
         - Add Delivery Note to Invoice.
     """,
-    "version": "15.0.1.0.0",
+    "version": "14.0.1.0.0",
     "License": "AGPL-3.0",
     "category": "Customer Relationship Management",
     "author": "Stephen Ngailo, StiloTech Limited",
     "application": True,
     "installable": True,
     "auto_install": False,
-    "depends": ["base","sale_management"],
+    "depends": ["base","sale_management","stock"],
     "data": ["views/res_partner.xml"],
     'images': ['static/description/icon.png'],
 }
