f702418ba8067df5d9bf19613e4deba71a3ccd92
Stephen Ngailo <92292992+StephenNgailo@users.noreply.github.com>
2021-12-03 15:17:07 +0300

    Update __manifest__.py

--- contacts_vrn/__manifest__.py
+++ contacts_vrn/__manifest__.py
@@ -1,13 +1,13 @@
 
 {
     "name": "Contact VRN/PON",
-    "summary": "Add VRN / Po No / DN fields ",
+    "summary": "Add VRN (Vat Registration Number) field to contacts / Add Buyer Order Number (Po No) to Sales Order and Invoice / Add Delivery Note to Invoice",
     'description': """
         - Add VRN (Vat Registration Number) field to contacts.
         - Add Buyer Order Number (Po No) to Sales Order and Invoice.
         - Add Delivery Note to Invoice.
     """,
-    "version": "14.0.1.0.0",
+    "version": "13.0.1.0.0",
     "License": "AGPL-3.0",
     "category": "Customer Relationship Management",
     "author": "Stephen Ngailo, StiloTech Limited",
